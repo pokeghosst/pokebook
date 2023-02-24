bffa52913d2a9254d05b0d2a2d55ac5bec62d7af
pokeghost <me@pokeghost.org>
2023-02-24 09:29:23 +0200

    fix: Saving empty poems Squished a nasty bug that allowed saving with empty poem and notes

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -4,17 +4,18 @@
 	import { db } from '../stores/db.js';
 
 	async function stashPoem() {
-		console.log('Stashing poem');
-		try {
-			await db.poems.add({
-				note: $noteStorage,
-				poem: $poemStorage,
-				timestamp: Date.now()
-			});
-			noteStorage.update(() => '');
-			poemStorage.update(() => '');
-		} catch (e) {
-			console.log(e);
+		if ($poemStorage != '' || $noteStorage != '') {
+			try {
+				await db.poems.add({
+					note: $noteStorage,
+					poem: $poemStorage,
+					timestamp: Date.now()
+				});
+				noteStorage.update(() => '');
+				poemStorage.update(() => '');
+			} catch (e) {
+				console.log(e);
+			}
 		}
 	}
 </script>
@@ -24,7 +25,11 @@
 		<li class="pr-5">
 			<a href="/"><img alt="Poke!Book" src="/logo.png" style="width: 200px" /></a>
 		</li>
-		<li class="pr-5 underline decoration-dotted hover:no-underline block"><a href="/" on:click={() => stashPoem()}>New poem</a></li>
-		<li><a href="/stash" class="underline decoration-dotted hover:no-underline block">Poem stash</a></li>
+		<li class="pr-5 underline decoration-dotted hover:no-underline block">
+			<a href="/" on:click={() => stashPoem()}>New poem</a>
+		</li>
+		<li>
+			<a href="/stash" class="underline decoration-dotted hover:no-underline block">Poem stash</a>
+		</li>
 	</ul>
 </div>
