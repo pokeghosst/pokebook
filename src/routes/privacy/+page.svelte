fa55c1d51f6b69a56ad21251de227e2b1b0589ac
Pokeghost <me@pokeghost.org>
2023-08-09 19:25:58 +0300

    chore: Update privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,7 +1,7 @@
 <div class="w-7/12 mt-10 mx-auto">
 	<h1 class="text-4xl text-center">Privacy Policy</h1>
 	<br />
-	<p class="font-bold">Last updated April 03, 2023</p>
+	<p class="font-bold">Last updated August 09, 2023</p>
 	<br />
 	<p>
 		This Privacy Policy applies to the PokeBook (the "App") and governs how it collects, uses, and
@@ -13,9 +13,11 @@
 	<p>
 		In short, PokeBook never requests any personal or identifiable information.. If you connect to
 		Google Drive, your poems and personal data are stored and processed by Google. Interface
-		settings (font, alignment, theme, etc.) are stored locally. By default, your poems are stored
-		locally. Regardless of your selected storage option, PokeBook never has access to the contents
-		of your poems or your perosnal information.
+		settings (font, alignment, theme, etc.) are stored locally. If you're using browser, they are
+		stored in the localStorage. On mobile devices, settings are stored in UserDefaults (on iOS) or
+		SharedPreferences (on Android). By default, your poems are stored locally. Regardless of your
+		selected storage option, PokeBook never has access to the contents of your poems or your
+		perosnal information.
 	</p>
 	<br />
 	<p>
@@ -26,9 +28,13 @@
 	<h2 class="text-xl">Where are poems stored?</h2>
 	<br />
 	<p>
-		PokeBook provides two possible storage options for poems: browser's storage and Google Drive.
-		When using the former, poems and notes are stored in your local IndexedDB instance. Thus, only
-		those who have acces to your device can access your poems.
+		PokeBook provides three possible storage options for poems: browser's storage (if you're using
+		it in browser), device file system (if you're using a mobile app from the store) and Google
+		Drive. When using the former, poems and notes are stored in your local IndexedDB instance. When
+		you're using a mobile app, poems are saved as regular text files on your device. PokeBook
+		doesn't read into your filesystem and only saves files in the application persistent folder.
+		Please note! If you uninstall the app, the poems will be gone. With these two options only those
+		who have acces to your device can access your poems.
 	</p>
 	<br />
 	<p>
@@ -40,13 +46,15 @@
 	<br />
 	<p>
 		When you use 'local' storage option, all information is stored and processed directly on your
-		device. No information about you or your device is collected.
+		device (either in browser or in file system). No information about you or your device is
+		collected.
 	</p>
 	<br />
 	<p>
 		When you use 'Google Drive' storage option, your authentication data is processed by Google.
-		PokeBook stores your 'refresh token' so you can save and load poems without having to
-		re-authenticate each time.
+		PokeBook stores your UUID (Universally unique identifier) that is passed to PokeBook's backend
+		service so that it knows it's you who's making the request. All this is needed to save and load
+		poems without having to re-authenticate each time.
 	</p>
 	<br />
 	<p>
@@ -55,9 +63,8 @@
 	</p>
 	<br />
 	<p>
-		In addition, when you use 'Google Drive' storage option, your browser's Request User Agent is
-		logged. Log retention is 4K lines and 1 hour of logs. This is necessary because PokeBook relies
-		on Vercel's serverless functoins to send requests to Google Drive API.
+		In addition, when you use 'Google Drive' storage option, short-living request logs are recorded
+		strictly for internal use to understand errors if they happen.
 	</p>
 	<br />
 	<h2 class="text-xl">What information is stored on your device?</h2>
@@ -74,6 +81,10 @@
 			<th>Purpose</th>
 		</tr>
 		<tr>
+			<td>dayTheme</td>
+			<td>The name of the currently active light theme</td>
+		</tr>
+		<tr>
 			<td>nightTheme</td>
 			<td>The name of the currently active dark theme</td>
 		</tr>
@@ -88,12 +99,6 @@
 		<tr>
 			<td>darkMode</td>
 			<td>Denotes whether the dark mode is active or not</td>
-		</tr>
-		<tr>
-			<td>refreshCode</td>
-			<td
-				>Refresh code used to generate authentication tokens for making requests to Google Drive</td
-			>
 		</tr>
 		<tr>
 			<td>viewsState</td>
@@ -120,8 +125,20 @@
 			<td>Active selected alignment option</td>
 		</tr>
 		<tr>
-			<td>dayTheme</td>
-			<td>The name of the currently active light theme</td>
+			<td>backupPoemText</td>
+			<td>The text of unsaved poem you edited</td>
+		</tr>
+		<tr>
+			<td>backupPoemName</td>
+			<td>The name of unsaved poem you edited</td>
+		</tr>
+		<tr>
+			<td>backupPoemNote</td>
+			<td>The note of unsaved poem you edited</td>
+		</tr>
+		<tr>
+			<td>pokehelp</td>
+			<td>PokeHelp writing mode</td>
 		</tr>
 	</table>
 	<br />
@@ -151,6 +168,13 @@
 		When 'local' storage option is selected, all poems are stored in the <em>poems</em> IndexedDB database
 		with the following columns. Storage for the database is permanent, it's cleared only when you clear
 		it yourself.
+	</p>
+	<br />
+	<h3 class="text-lg">Device File System</h3>
+	<br />
+	<p>
+		When 'local' storage option is selected in a mobile app, all poems are stored in the application
+		directory according to your operating system as plain text files.
 	</p>
 	<br />
 	<table>
