b9c7f4d3270c7306b09e38de1eb556cc89f7dc20
Pokeghost <me@pokeghost.org>
2023-10-09 19:09:05 +0300

    Add support link

---
+++ src/routes/support/+page.svelte
@@ -0,0 +1,21 @@
+<div class="w-11/12 mt-10 mx-auto">
+	<h1 class="text-2xl">Getting help</h1>
+	<br />
+	<p>
+		Encountered an issued when using PokeBook? No worries! You can use the following channels to get
+		support.
+	</p>
+    <br>
+	<p>Email: <a href="mailto:me@pokeghost.org" class="font-bold">me@pokeghost.org</a></p>
+	<p>Tag <a href="https://twitter.com/pok3ghost" class="font-bold">@pok3ghost</a> on X</p>
+	<p>
+		Open a GitHub issue: <a href="https://github.com/pokegh0st/pokebook" class="font-bold"
+			>https://github.com/pokegh0st/pokebook</a
+		>
+	</p>
+    <p>
+        Discord: send a DM to <strong>pokeghost</strong>
+    </p>
+    <br>
+    <p>All messages will be replied!</p>
+</div>
