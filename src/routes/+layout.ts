1fc56f1ac1c33445c768fdb2af96ae14d5510819
Pokeghost <me@pokeghost.org>
2023-11-22 15:01:41 +0200

    Can't use `window` when building (obv)

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -1,5 +1,4 @@
 import { loadTranslations } from '$lib/translations';
-import { Preferences } from '@capacitor/preferences';
 import '../sass/main.scss';
 
 export const prerender = true;
@@ -7,7 +6,7 @@ export const ssr = false;
 
 /** @type { import('@sveltejs/kit').Load } */
 export const load = async () => {
-	const lang = (await Preferences.get({ key: 'active_language' })).value || 'en';
+	const lang = 'en'
 
 	loadTranslations(lang);
 
