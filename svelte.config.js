e3b36489fd24b230d1e6972edbf83824535f154b
Pokeghost <me@pokeghost.org>
2023-11-25 10:23:14 +0200

    fix: Change AppFlow build command

--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,13 @@
 import dotenv from 'dotenv';
+
 dotenv.config();
 
 import adapterStatic from '@sveltejs/adapter-static';
 import adapteVercel from '@sveltejs/adapter-vercel';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-/** @type {import('@sveltejs/kit').Config} */
+/** @type {import("@sveltejs/kit").Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
 	preprocess: vitePreprocess(),
 
 	kit: {
