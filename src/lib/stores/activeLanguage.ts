184aba9c8475f45b372b825065655a44a9a98636
Pokeghost <pokeghost@disroot.org>
2024-02-01 19:31:56 +0200

    Language reactivity

--- src/lib/stores/activeLanguage.ts
+++ src/lib/stores/activeLanguage.ts
@@ -1,3 +1,3 @@
 import { createStore } from './storeFactory';
 
-export const activeLanguage = await createStore('active_language', navigator.language.split('-')[0]);
+export const activeLanguage = await createStore('active_language', '');
--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -4,15 +4,26 @@ import { Preferences } from '@capacitor/preferences';
 
 import { loadTranslations } from '$lib/translations';
 
+import { activeLanguage } from '$lib/stores/activeLanguage';
+
 import '../sass/main.scss';
 
 export const prerender = true;
 export const ssr = false;
 
 export const load = async () => {
-	const lang = browser
-		? (await Preferences.get({ key: 'active_language' })).value ?? navigator.language.split('-')[0]
-		: 'en';
+	let currentLanguageValue = (await Preferences.get({ key: 'active_language' })).value;
 
-	loadTranslations(lang);
+	console.log(currentLanguageValue);
+
+	if (!currentLanguageValue) {
+		const localeLanguage = navigator.language.split('-')[0];
+		console.log(navigator.language);
+		activeLanguage.set(localeLanguage);
+		currentLanguageValue = localeLanguage;
+	}
+
+	activeLanguage.subscribe((value) => {
+		loadTranslations(value);
+	});
 };
