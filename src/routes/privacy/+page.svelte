0424865e135f513af99d91d64b4ecdd05445c563
pokeghost <pokeghost@disroot.org>
2024-03-14 18:17:20 +0200

    chore: Update privacy policy

--- src/routes/privacy/+page.svelte
+++ src/routes/privacy/+page.svelte
@@ -1,60 +1,68 @@
 <div style="padding: 2rem">
 	<h1>Privacy Policy</h1>
-	<h2>Last updated February 7, 2024</h2>
+	<h2>Last updated March 14, 2024</h2>
 	<br />
 	<p>
 		This Privacy Policy applies to the PokeBook (the "App") and governs how it collects, uses, and
-		discloses information collected from users ("you" or "your") of the App. This policy aims to be
-		as simple and straightforward as possible so you clearly understand everything that is important
-		without unnecessary legal talk.
+		discloses information collected from users ("you" or "your") of the app. The app is provided by
+		Pokeghost (the "Developer"). This policy aims to be simple and straightforward, so you clearly
+		understand everything that is important without unnecessary legal talk.
 	</p>
 	<br />
+	<h2>PokeBook and your personal data</h2>
+	<br />
 	<p>
-		In short, PokeBook never requests any personal or identifiable information. If you log in with a
-		cloud provider, your poems and personal data are stored and processed by the provider. Interface
-		settings (font, alignment, theme, etc.) are stored locally. If you're using browser, they are
-		stored in the `localStorage`. On mobile devices, settings are stored in `UserDefaults` (on iOS)
-		or `SharedPreferences` (on Android). By default, your poems are stored locally. Regardless of
-		your selected storage option, PokeBook never has access to the contents of your poems or your
-		personal information. You can think of it as a text processing application.
+		PokeBook does not collect any personal or identifiable information. PokeBook allows you to write
+		poems and notes that are processed and stored on your device by default. Thus, no special
+		procedures are required.
 	</p>
 	<br />
+	<h2>Sharing with third-parties</h2>
+	<br />
 	<p>
-		PokeBook doesn't use any kind of analytics or session recording services (such as Goole
-		Analytics, Facebook Pixel, Hotjar, etc.). PokeBook is deployed on Vercel platform that
-		automatically keeps logs of requests. These logs include User Agent. No other information
-		related to you is recorded.
+		PokeBook provides OPTIONAL integration capabilities with third-party cloud providers for storing
+		poems and notes you write on their premises. When enabled, your poems and notes will be
+		processed by them, and you will be subject to their terms of service and privacy policies. When
+		choosing a third-party storage option, an access token, a special short-lived value, is stored
+		locally on your device for interacting with the service. It is deleted when you log out. When
+		connecting to a third-party service, PokeBook does not read your information, such as email,
+		name, etc., and cannot read the contents of other files and folders on your cloud drive. You can
+		find the full list of available storage providers in Settings.
 	</p>
 	<br />
-	<h2>Where are poems stored?</h2>
+
+	<h2>Your preferences and customizations</h2>
 	<br />
 	<p>
-		PokeBook provides three possible storage options for poems: browser's storage (if you're using
-		it in browser), device file system (if you're using a mobile app from the store) and cloud
-		storage provider. When using the former, poems and notes are stored in your local `IndexedDB`
-		instance. When you're using a mobile app, poems are saved as regular text files on your device.
-		PokeBook doesn't read into your filesystem and only saves files in the application persistent
-		folder. Please note! If you uninstall the app, the poems will be gone. With these two options
-		only those who have acces to your device can access your poems. On the other hand, when choosing
-		cloud provider, poems and notes are stored there. This data is subject to provider's terms of
-		service and privacy policy.
+		PokeBook allows setting a number of preferences, such as font, color theme, text justification,
+		etc. These preferences persist locally on your device and are never shared.
 	</p>
 	<br />
-	<h2>What information is processed and how?</h2>
+	<h2>Analytics and logs</h2>
 	<br />
 	<p>
-		When you use 'local' storage option, all information is stored and processed directly on your
-		device (either in browser or in file system). No information about you or your device is
-		collected.
+		PokeBook does not use analytics services. PokeBook's server provider keeps short-lived logs of
+		requests. These logs include the user agent. No other information related to you is included.
+		These logs are collected only for the web version.
 	</p>
 	<br />
+	<h2>Data retention and deletion</h2>
+	<br />
 	<p>
-		When you use one of the integrated providers, your authentication data is processed by it.
-		PokeBook has access only to files it created in your drive. PokeBook cannot access other files
-		you have in your drive.
+		Poems are stored for as long as you need them. Each poem can be deleted, and it is deleted
+		immediately. Preferences are stored in your device's long-term storage. Preferences can be
+		cleared by deleting the app or clearing the browser cache. No other data retention is done, so
+		no specific data deletion procedures are necessary.
 	</p>
 	<br />
 	<h2>Cookies</h2>
 	<br />
 	<p>PokeBook doesn't use cookies.</p>
+	<br />
+	<h2>Point of contact for inquires</h2>
+	<br />
+	<p>
+		For any questions or clarifications regarding the privacy policy, please send an email to
+		<u><b><a href="mailto:me@pokeghost.org">me@pokeghost.org</a></b></u>.
+	</p>
 </div>
