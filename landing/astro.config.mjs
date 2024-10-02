daade19c44d1de19446e8eaee3253fe0134a351b
pokeghost <pokeghost@disroot.org>
2024-10-02 21:54:00 +0300

    Remove Vercel image processing

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -7,7 +7,6 @@ import vercel from "@astrojs/vercel/static";
 export default defineConfig({
   output: "static",
   adapter: vercel({
-    imageService: true,
     webAnalytics: {
       enabled: true,
     },
