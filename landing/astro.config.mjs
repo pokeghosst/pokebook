94fdf7ec7beb6814d309ca0473631d26e99e32ab
pokeghost <pokeghost@disroot.org>
2024-10-02 21:36:54 +0300

    Config Vercel adapter

--- landing/astro.config.mjs
+++ landing/astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    imageService: true,
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+});
