352cde8452ee73a719e7f045c6423113312c3baf
pokeghost <pokeghost@disroot.org>
2025-12-11 17:31:21 +0200

    feat(omnisync): Add base and Google error classes

---
+++ omnisync/errors/GoogleError.ts
@@ -0,0 +1,25 @@
+/*
+@pokebook/omnisync -- Syncing backend for PokeBook
+Copyright (C) 2025 Pokeghost.
+
+omnisync is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published
+by the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+omnisync is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+*/
+
+import { ErrorBase } from "./ErrorBase";
+
+type ErrorName =
+  | "GOOGLE_REFRESH_MISSING_CREDENTIALS"
+  | "GOOGLE_CALLBACK_MISSING_TOKEN";
+
+export class GoogleError extends ErrorBase<ErrorName> {}
