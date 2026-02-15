0ce5cdd84d196a51dfca2330637cafb4cee2269e
pokeghost <pokeghost@disroot.org>
2026-02-15 18:42:16 +0200

    fix: Add input handlers for draft poem

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,31 +18,37 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 <script lang="ts">
 	import { sharePoem } from '$lib/actions/sharePoem';
+	import type { OnlyNote, OnlyPoem } from '$lib/schema/poem.schema';
+	import { savePoem } from '$lib/services/poem.service';
 	import {
 		draftPoemBodyStore,
 		draftPoemNameStore,
 		draftPoemNoteStore
 	} from '$lib/stores/poemDraft';
 	import { t } from '$lib/translations';
-	import type { ToolbarItem } from '$lib/types';
+	import type { InputChangeEvent, InputChangeHandler, ToolbarItem } from '$lib/types';
 	import { GLOBAL_TOAST_POSITION, GLOBAL_TOAST_STYLE } from '$lib/util/constants';
 	import hotkeys from 'hotkeys-js';
-	import { savePoem } from '$lib/services/poem.service';
 	import Save from 'lucide-svelte/icons/save';
 	import Share2 from 'lucide-svelte/icons/share-2';
 	import Trash2 from 'lucide-svelte/icons/trash-2';
 	import { onDestroy, onMount, setContext } from 'svelte';
 	import toast from 'svelte-french-toast';
-	import type { Writable } from 'svelte/store';
 	import Workspace from '../components/Workspace.svelte';
+	import { Preferences } from '$lib/plugins/Preferences';
 
-	setContext<{ name: Writable<string>; text: Writable<string> }>('poem', {
-		name: draftPoemNameStore,
-		text: draftPoemBodyStore
-	});
-	setContext<{ note: Writable<string> }>('note', { note: draftPoemNoteStore });
+	let thinking = $state(true);
 
+	let poem = $state<OnlyPoem>({ name: '', text: '' });
+	let note = $state<OnlyNote>({ note: '' });
 
+	setContext('poem', poem);
+	setContext('note', note);
+	setContext<[InputChangeHandler<HTMLInputElement>, InputChangeHandler<HTMLTextAreaElement>]>(
+		'poemHandlers',
+		[handleNameChange, handleTextChange]
+	);
+	setContext('noteHandler', handleNoteChange);
 
 	onMount(async () => {
 		hotkeys.filter = function () {
@@ -52,11 +58,38 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 			stashPoem();
 			return false;
 		});
+
+		const [name_, text_, note_] = await Promise.all([
+			Preferences.get({ key: 'draft_poem_name' }),
+			Preferences.get({ key: 'draft_poem_text' }),
+			Preferences.get({ key: 'draft_poem_note' })
+		]);
+
+		poem.name = name_.value ?? 'Unnamed';
+		poem.text = text_.value ?? '';
+		note.note = note_.value ?? '';
+
+		thinking = false;
 	});
 
 	onDestroy(() => {
 		hotkeys.unbind('ctrl+shift+n, command+shift+n');
 	});
+
+	async function handleNameChange(e: InputChangeEvent<HTMLInputElement>) {
+		poem.name = e.currentTarget.value;
+		await Preferences.set({ key: 'draft_poem_name', value: poem.name });
+	}
+
+	async function handleTextChange(e: InputChangeEvent<HTMLTextAreaElement>) {
+		poem.text = e.currentTarget.value;
+		await Preferences.set({ key: 'draft_poem_text', value: poem.text });
+	}
+
+	async function handleNoteChange(e: InputChangeEvent<HTMLInputElement>) {
+		note.note = e.currentTarget.value;
+		await Preferences.set({ key: 'draft_poem_note', value: note.note });
+	}
 
 	async function stashPoem() {
 		try {
@@ -112,4 +145,10 @@ along with this program. If not, see <https://www.gnu.org/licenses/>.
 	] satisfies ToolbarItem[]);
 </script>
 
-<Workspace {actions} />
+{#if thinking}
+	<div class="placeholder-text-wrapper">
+		<p>Loading...</p>
+	</div>
+{:else}
+	<Workspace {actions} />
+{/if}
