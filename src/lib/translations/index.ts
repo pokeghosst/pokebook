e88622769ce73ebe4f49f5302178640fb02e5462
pokeghost <pokeghost@disroot.org>
2026-03-26 14:35:41 +0200

    chore: Format unlinted files

--- src/lib/translations/index.ts
+++ src/lib/translations/index.ts
@@ -18,30 +18,66 @@ const config = {
 		// en
 		{ locale: 'en', key: 'about', loader: async () => (await import('./en/about.json')).default },
 		{ locale: 'en', key: 'errors', loader: async () => (await import('./en/errors.json')).default },
-		{ locale: 'en', key: 'hotkeys', loader: async () => (await import('./en/hotkeys.json')).default },
+		{
+			locale: 'en',
+			key: 'hotkeys',
+			loader: async () => (await import('./en/hotkeys.json')).default
+		},
 		{ locale: 'en', key: 'menu', loader: async () => (await import('./en/menu.json')).default },
-		{ locale: 'en',	key: 'settings', loader: async () => (await import('./en/settings.json')).default },
+		{
+			locale: 'en',
+			key: 'settings',
+			loader: async () => (await import('./en/settings.json')).default
+		},
 		{ locale: 'en', key: 'themes', loader: async () => (await import('./en/themes.json')).default },
 		{ locale: 'en', key: 'toasts', loader: async () => (await import('./en/toasts.json')).default },
-		{ locale: 'en', key: 'workspace', loader: async () => (await import('./en/workspace.json')).default },
+		{
+			locale: 'en',
+			key: 'workspace',
+			loader: async () => (await import('./en/workspace.json')).default
+		},
 		// es
 		{ locale: 'es', key: 'about', loader: async () => (await import('./es/about.json')).default },
 		{ locale: 'es', key: 'errors', loader: async () => (await import('./es/errors.json')).default },
-		{ locale: 'es', key: 'hotkeys', loader: async () => (await import('./es/hotkeys.json')).default },
+		{
+			locale: 'es',
+			key: 'hotkeys',
+			loader: async () => (await import('./es/hotkeys.json')).default
+		},
 		{ locale: 'es', key: 'menu', loader: async () => (await import('./es/menu.json')).default },
-		{ locale: 'es',	key: 'settings', loader: async () => (await import('./es/settings.json')).default },
+		{
+			locale: 'es',
+			key: 'settings',
+			loader: async () => (await import('./es/settings.json')).default
+		},
 		{ locale: 'es', key: 'themes', loader: async () => (await import('./es/themes.json')).default },
 		{ locale: 'es', key: 'toasts', loader: async () => (await import('./es/toasts.json')).default },
-		{ locale: 'es', key: 'workspace', loader: async () => (await import('./es/workspace.json')).default },
+		{
+			locale: 'es',
+			key: 'workspace',
+			loader: async () => (await import('./es/workspace.json')).default
+		},
 		// ru
 		{ locale: 'ru', key: 'about', loader: async () => (await import('./ru/about.json')).default },
 		{ locale: 'ru', key: 'errors', loader: async () => (await import('./ru/errors.json')).default },
-		{ locale: 'ru', key: 'hotkeys', loader: async () => (await import('./ru/hotkeys.json')).default },
+		{
+			locale: 'ru',
+			key: 'hotkeys',
+			loader: async () => (await import('./ru/hotkeys.json')).default
+		},
 		{ locale: 'ru', key: 'menu', loader: async () => (await import('./ru/menu.json')).default },
-		{ locale: 'ru',	key: 'settings', loader: async () => (await import('./ru/settings.json')).default },
+		{
+			locale: 'ru',
+			key: 'settings',
+			loader: async () => (await import('./ru/settings.json')).default
+		},
 		{ locale: 'ru', key: 'themes', loader: async () => (await import('./ru/themes.json')).default },
 		{ locale: 'ru', key: 'toasts', loader: async () => (await import('./ru/toasts.json')).default },
-		{ locale: 'ru', key: 'workspace', loader: async () => (await import('./ru/workspace.json')).default },
+		{
+			locale: 'ru',
+			key: 'workspace',
+			loader: async () => (await import('./ru/workspace.json')).default
+		}
 	]
 } satisfies Config;
 
