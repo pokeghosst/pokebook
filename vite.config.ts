d8997960d4b45d2b0b72eca38fcab17a7717098e
pokeghost <pokeghost@disroot.org>
2026-08-03 18:13:02 +0300

    feat: add SvelteKitPWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 import devtoolsJson from 'vite-plugin-devtools-json';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
-	plugins: [sveltekit(), devtoolsJson()],
+	plugins: [sveltekit(), SvelteKitPWA(), devtoolsJson()],
 	server: {
 		watch: {
 			ignored: ['**/src-tauri/**']
